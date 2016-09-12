5f0dc5ad4fb4098ce2d444abbf28fb28bb87d0f7
Zi Ischuros <zischuros@users.noreply.github.com>
2016-09-12 13:52:42 -0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,40 @@
+// operands
+var zero = document.getElementById('zero');
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+var four = document.getElementById('four');
+var five = document.getElementById('five');
+var six = document.getElementById('six');
+var seven = document.getElementById('seven');
+var eight = document.getElementById('eight');
+var nine = document.getElementById('nine');
+//
+// operations
+var add = document.getElementById('add');
+var subtract = document.getElementById('subtract');
+var multiply = document.getElementById('multiply');
+var divide = document.getElementById('divide');
+//
+// enter
+var enter = document.getElementById('enter');
+// display
+var display = document.getElementById('display');
+// clear
+var clear = document.getElementById('clear');
+// number keys
+var numkeys = [zero, one, two, three, four, five, six, seven, eight, nine];
+//
+var operators = [{
+    operation: add
+    , opera: '+'
+}, {
+    operation: subtract
+    , opera: '-'
+}, {
+    operation: multiply
+    , opera: '*'
+}, {
+    operation: divide
+    , opera: '/'
+}];
